38e52c3694b2e73e952548322ff09b433de61b35
Ammon Morris <likesalmon@gmail.com>
2024-04-08 10:39:07 -0800

    feat(SpeedInsights): install SpeedInsights
    Nest it inside the body tag to avoid `Error: Hydration failed because the initial UI does not match what was rendered on the server`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import Navigation from '@/components/Navigation';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
       >
         <Navigation />
         <div className="p-12">{children}</div>
+        <SpeedInsights />
       </body>
     </html>
   );
